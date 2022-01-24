61d7a894cc9242e82c94bcd1dbbee68fd7475dc7
thirugnanasambandham <kvthiru95@gmail.com>
2022-01-24 21:48:17 +0530

    taskdone

---
+++ src/org/amazon/Amazon.java
@@ -0,0 +1,54 @@
+package org.amazon;
+
+public class Amazon {
+	private void login() {
+System.out.println("amozon page");
+}
+	private void username() {
+System.out.println("login page");
+
+	}
+private void password() {
+	System.out.println("password");
+
+}	
+public static void main(String[] args) {
+	
+	Amazon a=new Amazon();
+	a.login();
+	a.username();
+	a.password();
+	
+	
+	
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
